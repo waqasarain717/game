9776f2ef3a04dcbf6e980215b06a57908c64fa37
waqasarain717 <93517507+waqasarain717@users.noreply.github.com>
2021-11-01 14:27:56 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,89 @@
+score = 0
+cross = true;
+
+
+document.onkeydown = function (e) {
+    console.log("key Code is: ", e.keyCode)
+    if (e.keyCode == 38) {
+        dino = document.querySelector('.dino');
+        dino.classList.add('animateDino');
+        setTimeout(() => {
+            dino.classList.remove('animateDino')
+        }, 700);
+    }
+    if (e.keyCode == 39) {
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = dinoX + 112 + "px";
+    }
+    if (e.keyCode == 37) {
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = (dinoX - 12) + "px";
+    }
+}
+setInterval(() => {
+    dino = document.querySelector('.dino');
+    gameOver = document.querySelector('.gameOver');
+    obstacle = document.querySelector('.obstacle');
+
+    dx = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+    dy = parseInt(window.getComputedStyle(dino, null).getPropertyValue('top'));
+    ox = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('left'));
+    oy = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('top'));
+
+    offsetX = Math.abs(dx - ox);
+    offsetY = Math.abs(dy - oy);
+    if (offsetX < 73 && offsetY < 52) {
+        gameOver.style.visibility = 'visible';
+
+        obstacle.classList.remove('obstacleAni')
+
+    }
+    else if (offsetX < 145 && cross) {
+        score += 1;
+        updateScore(score);
+        cross = false;
+        setTimeout(() => {
+            cross = true
+        }, 1000);
+        setTimeout(() => {
+            aniDur = parseFloat(window.getComputedStyle(obstacle, null).getPropertyValue('animation-duration'));
+            newDur = aniDur - 0.1;
+            obstacle.style.animationDuration = newDur + 's';
+        }, 500);
+
+    }
+
+}, 10);
+function updateScore(score) {
+    scoreCont.innerHTML = "Your Score: " + score
+}
+function refresh() {
+    setTimeout(function () {
+        location.reload()
+        play.classList.visibility = 'visible';
+    }, 1);
+}
+function left() {
+    dino = document.querySelector('.dino');
+    dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+    dino.style.left = (dinoX - 12) + "px";
+}
+
+function up() {
+    dino = document.querySelector('.dino');
+    dino.classList.add('animateDino');
+    setTimeout(() => {
+        dino.classList.remove('animateDino')
+    }, 700);
+}
+function right() {
+
+    console.log('left')
+    dino = document.querySelector('.dino');
+    dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+    dino.style.left = dinoX + 112 + "px";
+}
+
+
